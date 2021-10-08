3ae24b300ff89061d9deba90346940378d7297bc
kthss01 <kthss01@naver.com>
2021-10-08 11:07:49 +0900

    vuejs 3 개발 환경 구축 및 컴포넌트 사용해보기

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="name">
+    {{ name }}
+    <!-- {{ greeting(name) }} -->
+    <!-- {{ greet }} -->
+    {{ arr }}
+  </div>
+
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+    Click
+  </button>
+</template>
+
+<script>
+import { ref } from 'vue';
+// import { ref, reactive } from 'vue';
+export default {
+  setup() {
+    const name = ref('Kossie Coder1');
+
+    // const arr = reactive({
+    //   id: 1
+    // });
+
+    const arr = ref({
+      id: 1
+    })
+
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name;
+    // };
+
+    // const greet = greeting(name);
+
+    // const consoleLog = () => {
+    //   console.log('hello world');
+    // };
+
+    const updateName = () => {
+      name.value = "Kossie Coder";
+      // arr.id = 2;
+      arr.value.id = 2; // ref 쓰면
+      console.log(name);
+    }
+
+    return {
+      name,
+      arr,
+      // greeting,
+      // greet,
+      // consoleLog,
+      updateName,
+    };
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>
