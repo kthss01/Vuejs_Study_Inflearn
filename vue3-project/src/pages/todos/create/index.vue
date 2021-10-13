4354220ac0e523c871b3628f1de187aec8914a66
kthss01 <kthss01@naver.com>
2021-10-13 21:22:49 +0900

    To-Do 생성 페이지 만들기

--- vue3-project/src/pages/todos/create/index.vue
+++ vue3-project/src/pages/todos/create/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <h1>To-Do Page</h1>
-    <TodoForm :editing="true" />
+    <h1>Create Todo</h1>
+    <TodoForm />
 </template>
 
 <script>
@@ -9,7 +9,7 @@ import TodoForm from '@/components/TodoForm.vue';
 export default {
     components: {
         TodoForm,
-    },
+    }
 }
 </script>
 
--- vue3-project/src/pages/todos/index.vue
+++ vue3-project/src/pages/todos/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>To-Do List</h1>
+    <div class="d-flex justify-content-between mb-3">
+      <h2>To-Do List</h2>
+      <button 
+        class="btn btn-primary"
+        @click="moveToCreatePage"
+      >
+          Create Todo
+      </button>
+    </div>
+
     <input 
       class="form-control"
       type="text" 
@@ -9,8 +18,6 @@
       @keyup.enter="searchTodo"
     >
     <hr />
-    <TodoSimpleForm @add-todo="addTodo" />
-    <div style="color: red">{{ error }}</div>
 
     <div v-if="!todos.length">
       There is nothing to display
@@ -55,19 +62,19 @@
  
 <script>
 import { ref, computed, watch } from 'vue';
-import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
 import TodoList from '@/components/TodoList.vue';
 import axios from 'axios';
 import Toast from '@/components/Toast.vue';
 import { useToast } from '@/composables/toast';
+import { useRouter } from 'vue-router'
 
 export default {
   components: {
-    TodoSimpleForm,
     TodoList,
     Toast,
   },
   setup() {
+    const router = useRouter();
     const todos = ref([]);
     const error = ref('');
     const numberOfTodos = ref(0);
@@ -147,6 +154,12 @@ export default {
       }
     };
 
+    const moveToCreatePage = () => {
+      router.push({
+        name: 'TodoCreate',
+      });
+    };
+
     let timeout = null;
     const searchTodo = () => {
       clearTimeout(timeout);
@@ -184,6 +197,7 @@ export default {
       showToast,
       toastMessage,
       toastAlertType,
+      moveToCreatePage,
     };
   }
 }
